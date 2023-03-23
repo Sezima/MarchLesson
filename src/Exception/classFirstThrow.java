93f08f6d13b90d1ee8b7b6d82b3a1b935c405991
sezim <osmsezimka@gmail.com>
2023-03-23 10:19:45 +0600

    we use throw

---
+++ src/Exception/classFirstThrow.java
@@ -0,0 +1,31 @@
+package Exception;
+
+public class classFirstThrow {
+    /*
+    The throw statement allows you to create a custom error.
+
+The throw statement is used together with an exception type. There are many exception types available in Java: ArithmeticException, FileNotFoundException, ArrayIndexOutOfBoundsException, SecurityException, etc:
+    */
+
+    static void checkAge(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        }
+        else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkAge(20); // Set age to 15 (which is below 18...)
+    }
+
+
+    /*
+    *Exception in thread "main" java.lang.ArithmeticException: Access denied - You must be at least 18 years old.
+        at Main.checkAge(Main.java:4)
+        at Main.main(Main.java:12)
+        *
+        if 20  Access granted - You are old enough!*/
+
+}
